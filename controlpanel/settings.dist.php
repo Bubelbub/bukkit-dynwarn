73a4e038aba535821f4e41a825eabb867b0552fa
Bubelbub <bubelbub@gmail.com>
2013-01-21 22:14:19 +0100

    Added security things

---
+++ controlpanel/settings.dist.php
@@ -0,0 +1,15 @@
+<?php
+// You could use the settings which you use in the plugin configuration
+define('MySQL_server',					'localhost');
+define('MySQL_port',					3306);
+define('MySQL_database',				'database');
+define('MySQL_username',				'root');
+define('MySQL_password',				'');
+define('MySQL_prefix',					'ew_');
+define('MySQL_optional_remove_entries',	true);
+
+// Globals
+define('CP_LANGUAGE', 'en'); // en = english, de = deutsch
+
+// if you use authme (or other authentications plugin) you could set it up here
+// -- coming soon! very soon! --
